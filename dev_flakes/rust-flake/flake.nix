9a519328c99f3cbe1f41362895c147a610d4748b
lofiwatercat <alan001tk@gmail.com>
2023-09-14 22:20:33 -0700

    update rust flake again

--- dev_flakes/rust-flake/flake.nix
+++ dev_flakes/rust-flake/flake.nix
@@ -4,7 +4,7 @@
   inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
 
   outputs = { self, nixpkgs }: {
-    devShell.x86_64-linux = let
+    devShells.x86_64-linux = let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
     in pkgs.mkShell {
