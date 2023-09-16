6a788b08ce604681524e364b5dae263631ca4579
lofiwatercat <alan001tk@gmail.com>
2023-09-15 22:09:22 -0700

    Add lock files

--- dev_flakes/ash-flake/flake.nix
+++ dev_flakes/ash-flake/flake.nix
@@ -2,34 +2,35 @@
   description = "A flake for Ash development";
 
   inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs }: {
-    devShell.x86_64-linux = let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-    in pkgs.mkShell {
-      buildInputs = with pkgs; [
-        rustc
-        cargo
-        rust-analyzer
-        vulkan-tools
-        vulkan-loader
-        vulkan-headers
-        vulkan-caps-viewer
-        vulkan-tools-lunarg
-        vulkan-validation-layers
-        libxkbcommon
-        libGL
-        udev
-        udev
-        alsa-lib
-        openssl
-        pkg-config
-      ];
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in with pkgs; {
+        devShell = mkShell rec {
+          buildInputs = [
+            rustc
+            cargo
+            rust-analyzer
+            vulkan-tools
+            vulkan-loader
+            vulkan-headers
+            vulkan-caps-viewer
+            vulkan-tools-lunarg
+            vulkan-validation-layers
+            libxkbcommon
+            libGL
+            udev
+            udev
+            alsa-lib
+            openssl
+            pkg-config
+          ];
+          LD_LIBRARY_PATH = nixpkgs.lib.makeLibraryPath buildInputs;
+        };
+          # defaultPackage.x86_64-linux = null;
+      });
 
-      # LD_LIBRARY_PATH = nixpkgs.lib.makeLibraryPath buildInputs;
-    };
-
-    defaultPackage.x86_64-linux = null;
-  };
 }
